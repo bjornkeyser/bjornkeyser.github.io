c4fcb1f3ce8ae165690e8acab182a772475a5492
bjornkeyser <bjornkeyser@gmail.com>
2023-11-09 22:28:25 +0100

    image thumbnails and moved the timestamp to the left of timeline

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-      // Updated categories data to match your new data-categories
-      const projectsData = {
-        'design-code': [
-            { title: "breath affects", id: 4, image_url: "Content/breath.gif", like_factor: 8, content_file: "breathaffects.html", date:"2022-03-24"},
-            { title: "flocka", id: 8, image_url: "Content/flockfuck1.png", like_factor: 8, content_file: "flocka.html", date:""},
-            { title: "TUNER", id: 15, image_url: "Content/tuner.png", like_factor: 8, content_file: "tuner.html", date:"2022-08-08"},
-            { title: "cca", id: 9, image_url: "Content/cca.png", like_factor: 8, content_file: "cca.html", date:"2022-08-10"}],
-        'data-visualization-interaction': [
-            { title: "spatial whispers", id: 5, image_url: "Content/sonicwhispers.png", like_factor: 5, content_file: "spatialwhispers.html", date:"2022-04-26"}, 
-            { title: "textual tension", id: 6, image_url: "Content/textualtension3.png", like_factor: 8, content_file: "textualtension.html", date:"2022-06-09" },
-            { title: "metaWHAT!?", id: 7, image_url: "Content/metawhat1.jpeg", like_factor: 8, content_file: "metawhat.html", date:"2022-06-01"}
-    ],
-        'consciousness': [
-            { title: "screaming plants", id: 11, image_url: "", like_factor: 7, content_file: "screamingplants.html", date:"2023-02-01"},
-            { title: "delay", id: 12, image_url: "", like_factor: 9, content_file: "delay.html", date:"2023-07-06"},
-            { title: "human magnetoreception - draft", id: 13, image_url: "", like_factor: 9, content_file: "magnetoreception.html", date:""},
-            { title: "physarum", id: 14, image_url: "", like_factor: 9, content_file: "physarum.html", date:"2022-09-26"},
-    
-        ],
-        'ai-creativity': [
-            { title: "blackout poetry", id: 1, image_url: "Content/arraysure1.png", like_factor: 7.5, content_file: "blackout.html", date:"2022-10-15"},
-            { title: "haikuki", id: 2, image_url: "Content/haikuki_thumbnail2.png", like_factor: 9, content_file: "haikuki.html", date:"2022-11-02"},
-            { title: "reversing roles", id: 3, image_url: "Content/rr1.jpeg", like_factor: 7.75, content_file: "reversingroles.html", date: "2022-12-30" },
-            { title: "split learning", id: 10, image_url: "Content/splitlearning.png", like_factor: 3, content_file: "splitlearning.html", date:"2021-07-01"}
-        ]
-      }; 
+// Updated categories data to match your new data-categories
+const projectsData = {
+'design-code': [
+    { title: "breath affects", id: 4, image_url: "Content/breath.gif", like_factor: 8, content_file: "breathaffects.html", date:"2022-03-24"},
+    { title: "flocka", id: 8, image_url: "Content/flockfuck1.png", like_factor: 8, content_file: "flocka.html", date:""},
+    { title: "TUNER", id: 15, image_url: "Content/tuner.png", like_factor: 8, content_file: "tuner.html", date:"2022-08-08"},
+    { title: "cca", id: 9, image_url: "Content/cca.png", like_factor: 8, content_file: "cca.html", date:"2022-08-10"}],
+'data-visualization-interaction': [
+    { title: "spatial whispers", id: 5, image_url: "Content/sonicwhispers.png", like_factor: 5, content_file: "spatialwhispers.html", date:"2022-04-26"}, 
+    { title: "textual tension", id: 6, image_url: "Content/textualtension3.png", like_factor: 8, content_file: "textualtension.html", date:"2022-06-09" },
+    { title: "metaWHAT!?", id: 7, image_url: "Content/metawhat1.jpeg", like_factor: 8, content_file: "metawhat.html", date:"2022-06-01"}
+],
+'consciousness': [
+    { title: "screaming plants", id: 11, image_url: "", like_factor: 7, content_file: "screamingplants.html", date:"2023-02-01"},
+    { title: "delay", id: 12, image_url: "", like_factor: 9, content_file: "delay.html", date:"2023-07-06"},
+    { title: "human magnetoreception - draft", id: 13, image_url: "", like_factor: 9, content_file: "magnetoreception.html", date:""},
+    { title: "physarum", id: 14, image_url: "", like_factor: 9, content_file: "physarum.html", date:"2022-09-26"},
+
+],
+'ai-creativity': [
+    { title: "blackout poetry", id: 1, image_url: "Content/arraysure1.png", like_factor: 7.5, content_file: "blackout.html", date:"2022-10-15"},
+    { title: "haikuki", id: 2, image_url: "Content/haikuki_thumbnail2.png", like_factor: 9, content_file: "haikuki.html", date:"2022-11-02"},
+    { title: "reversing roles", id: 3, image_url: "Content/rr1.jpeg", like_factor: 7.75, content_file: "reversingroles.html", date: "2022-12-30" },
+    { title: "split learning", id: 10, image_url: "Content/splitlearning.png", like_factor: 3, content_file: "splitlearning.html", date:"2021-07-01"}
+]
+}; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const bioColumn = document.getElementById('aboutText');
@@ -109,44 +109,133 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
 
-    // timeline stuff
-    const items = document.querySelectorAll('li[data-date]');
-    const timeline = document.querySelector('.timeline');
-    
-    // Convert the date attributes to actual Date objects
-    const dates = Array.from(items).map(item => new Date(item.dataset.date));
-    
-    // Find the max and min dates
-    const maxDate = new Date(Math.max.apply(null, dates));
-    const minDate = new Date(Math.min.apply(null, dates));
-    
-    // Calculate the total date range
-    const dateRange = maxDate - minDate;
-    
-    items.forEach(item => {
-        // Create a dot for each item
-        const dot = document.createElement('div');
-        dot.classList.add('timeline-dot');
-    
-        // Get the item's date
-        const itemDate = new Date(item.dataset.date);
-    
-        // Calculate the position of the dot based on the date
-        const position = ((maxDate - itemDate) / dateRange) * 95;
-        dot.style.top = `${position}%`;
-    
-        // Append the dot to the timeline
-        timeline.appendChild(dot);
-    
-        // Event listener for clicks
-        item.addEventListener('click', function() {
-            // Remove active class from all dots
-            document.querySelectorAll('.timeline-dot').forEach(dot => dot.classList.remove('active'));
-    
-            // Add active class to clicked item's dot
-            dot.classList.add('active');
-        });
-    });    
+    function displayTimeLine() {
+        // timeline stuff
+        const items = document.querySelectorAll('li[data-date]');
+        const timeline = document.querySelector('.timeline');
+        
+        // Convert the date attributes to actual Date objects
+        const dates = Array.from(items).map(item => new Date(item.dataset.date));
+        
+        // Find the max and min dates
+        const maxDate = new Date(Math.max.apply(null, dates));
+        const minDate = new Date(Math.min.apply(null, dates));
+        
+        // Calculate the total date range
+        const dateRange = maxDate - minDate;
+        
+        items.forEach(item => {
+            // Create a container for the dot and the timestamp
+            const entry = document.createElement('div');
+            entry.classList.add('timeline-entry');
+        
+            // Create a timestamp for each item
+            const timestamp = document.createElement('div');
+            timestamp.classList.add('timeline-timestamp');
+            timestamp.textContent = item.dataset.date; // Set the text to the item's date
+            timestamp.style.display = 'none'; // Initially hide the timestamp
+        
+            // Create a dot for each item
+            const dot = document.createElement('div');
+            dot.classList.add('timeline-dot');
 
+            // Extract the project ID from the data attribute
+            const projectId = item.getAttribute('data-project-id');
+            
+            // Set the project ID on the dot
+            dot.setAttribute('data-project-id', projectId);
+
+            // Add click event listener to the dot
+            dot.addEventListener('click', function() {
+                // Remove active class from all dots
+                document.querySelectorAll('.timeline-dot').forEach(dot => {
+                    dot.classList.remove('active');
+                    dot.previousSibling.style.display = 'none'; // Assuming the next sibling is the timestamp
+                });
+
+                // Add active class to clicked dot
+                this.classList.add('active');
+                this.previousSibling.style.display = 'block'; // Show the timestamp
+
+                // Load the project details
+                const projectId = this.getAttribute('data-project-id');
+                if (projectId) {
+                    loadProjectDetails(projectId);
+                }
+            });
+
+            // Get the item's date
+            const itemDate = new Date(item.dataset.date);
+        
+            // Calculate the position of the dot based on the date
+            const position = ((maxDate - itemDate) / dateRange) * 95;
+            entry.style.top = `${position}%`;
+        
+            // Append the timestamp and the dot to the entry
+            entry.appendChild(timestamp); // Timestamp first
+            entry.appendChild(dot);       // Dot second
+        
+            // Append the entry to the timeline
+            timeline.appendChild(entry);
+        
+            // Event listener for clicks
+            item.addEventListener('click', function() {
+                // Remove active class from all dots and hide all timestamps
+                document.querySelectorAll('.timeline-entry').forEach(entry => {
+                    entry.querySelector('.timeline-dot').classList.remove('active');
+                    entry.querySelector('.timeline-timestamp').style.display = 'none';
+                });
+        
+                // Add active class to clicked item's dot and show the timestamp
+                dot.classList.add('active');
+                timestamp.style.display = 'block'; // Show only the active item's timestamp
+                timestamp.style.backgroundColor = 'white';
+            });
+        });
+    }
+
+    displayTimeLine();
+
+    document.querySelectorAll('#projectsList li').forEach(item => {
+        item.addEventListener('mouseenter', function() {
+          // Retrieve the project ID from the data attribute of the hovered list item
+          const projectId = this.getAttribute('data-project-id');
+      
+          // Find the project data by searching through each category
+          let projectData;
+          for (const category in projectsData) {
+            projectData = projectsData[category].find(project => project.id.toString() === projectId);
+            if (projectData) {
+              break; // If we've found the project, exit the loop
+            }
+          }
+      
+          if (projectData && projectData.image_url) {
+            const imagePreview = document.getElementById('imagePreview');
+            // Clear previous content
+            imagePreview.innerHTML = '';
+
+            // Create a new img element
+            const img = document.createElement('img');
+            img.src = projectData.image_url;
+            img.style.width = '100%'; // or any other styles to fit the image as needed
+            img.style.height = 'auto';
+            img.style.maxHeight = '100%';
+            
+            // Append the new img element
+            imagePreview.appendChild(img);
+            imagePreview.style.display = 'block'; // Show the image container
+          }
+        });
+            
+        item.addEventListener('mouseleave', function() {
+          // On mouse leave, hide the image container and clear the background
+          const imagePreview = document.getElementById('imagePreview');
+          imagePreview.style.display = 'none'; // Hide the image container
+          imagePreview.style.backgroundImage = ''; // Clear the background image
+        });
+      });
+      
+            
   });
   
